35e1e8335ba7ab541f39ca45090c4d61f5c4f762
Andre Vanha <andre@vanhas.com>
2022-08-02 00:58:48 -0600

    Support for configuring the server

--- pmaas.go
+++ pmaas.go
@@ -1,29 +1,40 @@
 package core
 
 import (
-  "fmt"
-  "net/http"
-  "pmaas.io/spi"
+	"fmt"
+	"net/http"
+	"pmaas.io/spi"
 )
 
-type PMAAS struct {
-  plugins []spi.IPMAASPlugin
+type Config struct {
+	HttpPort int
 }
 
-func NewPMAAS() *PMAAS {
-  return &PMAAS{}
+func NewConfig() *Config {
+	return &Config{
+		8090,
+	}
+}
+
+type PMAAS struct {
+	config  *Config
+	plugins []spi.IPMAASPlugin
+}
+
+func NewPMAAS(config *Config) *PMAAS {
+	return &PMAAS{config: config}
 }
 
 func hello(w http.ResponseWriter, req *http.Request) {
-  fmt.Fprintf(w, "hello from pmaas\n")
+	fmt.Fprintf(w, "hello from pmaas\n")
 }
 
 func listPlugins(w http.ResponseWriter, req *http.Request) {
-  fmt.Fprintf(w, "plugins:\n")
+	fmt.Fprintf(w, "plugins:\n")
 }
 
-func (*PMAAS) ListenAndServe() {
-  http.HandleFunc("/", hello)
-  http.HandleFunc("/plugin", listPlugins)
-  http.ListenAndServe(":8090", nil)
+func (pmaas *PMAAS) ListenAndServe() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/plugin", listPlugins)
+	http.ListenAndServe(fmt.Sprintf(":%d", pmaas.config.HttpPort), nil)
 }
