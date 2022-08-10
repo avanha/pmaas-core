ced33bf7c877cebabd5f453e4357f5ad813f6fd7
Andre Vanha <andre@vanhas.com>
2022-08-10 10:10:23 -0600

    Implemented clean shutdown handling that stops plugins and the web server

--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.18
 
 require pmaas.io/spi v0.0.0
 
+require golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
+
 replace pmaas.io/spi => ../pmaas-spi
--- pmaas.go
+++ pmaas.go
@@ -1,8 +1,14 @@
 package core
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
 
 	"pmaas.io/spi"
 )
@@ -51,7 +57,7 @@ type PMAAS struct {
 
 func NewPMAAS(config *Config) *PMAAS {
 	return &PMAAS{
-		config: config,
+		config:  config,
 		plugins: config.plugins,
 	}
 }
@@ -65,6 +71,12 @@ func listPlugins(w http.ResponseWriter, req *http.Request) {
 }
 
 func (pmaas *PMAAS) Run() {
+	mainCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer func() {
+		fmt.Printf("Executing deferred stop()\n")
+		stop()
+	}()
+
 	fmt.Printf("Initializing...\n")
 	// Init plugins
 	for _, plugin := range pmaas.plugins {
@@ -79,7 +91,67 @@ func (pmaas *PMAAS) Run() {
 	}
 
 	fmt.Printf("Running...\n")
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/plugin", listPlugins)
-	http.ListenAndServe(fmt.Sprintf(":%d", pmaas.config.HttpPort), nil)
+	//http.HandleFunc("/", hello)
+	//http.HandleFunc("/plugin", listPlugins)
+
+	g, gCtx := errgroup.WithContext(mainCtx)
+
+	var httpServer = &http.Server{
+		Addr: fmt.Sprintf(":%d", pmaas.config.HttpPort),
+	}
+
+	g.Go(func() error {
+		fmt.Printf("HTTP Server starting...\n")
+		var err = httpServer.ListenAndServe()
+
+		if err == nil || err == http.ErrServerClosed {
+			fmt.Printf("HTTP Server stopped...\n")
+			err = nil
+		} else {
+			fmt.Printf("HTTP Server stopped with error: %s\n", err)
+		}
+		return err
+	})
+
+	time.Sleep(100 * time.Millisecond)
+
+	g.Go(func() error {
+		fmt.Printf("Shutdown task waiting for signal...\n")
+		<-gCtx.Done()
+		shutdownHttp(httpServer)
+		stopPlugins(pmaas.plugins)
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		fmt.Printf("Stopped with error: %s \n", err)
+
+	}
+
+	go func() {
+		<-mainCtx.Done()
+		fmt.Printf("mainCtx is done\n")
+	}()
+
+	fmt.Printf("Done\n")
+}
+
+func shutdownHttp(httpServer *http.Server) {
+	fmt.Printf("HTTP Server shutdown started...\n")
+	var err = httpServer.Shutdown(context.Background())
+
+	if err == nil {
+		fmt.Printf("HTTP Server shutdown complete...\n")
+	} else {
+		fmt.Printf("HTTP Server shutdown completd with error: %s\n", err)
+	}
+}
+
+func stopPlugins(plugins []*pluginWithConfig) {
+	fmt.Printf("Plugin shutdown started...\n")
+	// Stop plugins
+	for _, plugin := range plugins {
+		plugin.instance.Stop()
+	}
+	fmt.Printf("Plugin shutdown complete...\n")
 }
